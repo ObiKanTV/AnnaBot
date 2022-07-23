b8235c8b157c0cd8728d16c9d1b5562fcf552507
Kai Tapper <kai.tapper@outlook.com>
2022-07-23 19:21:52 +0200

    I am confuse

--- AnnaBot.App/CommandHandler.cs
+++ AnnaBot.App/CommandHandler.cs
@@ -5,21 +5,47 @@ namespace AnnaBot.App
     public class CommandHandler
     {
         private readonly CommandService _commands;
+        private readonly IServiceProvider _services;
         private readonly DiscordSocketClient _client;
+        private readonly LoggingService _log; 
 
-        public CommandHandler(DiscordSocketClient client, CommandService commands)
+        public CommandHandler(DiscordSocketClient client, CommandService commands, IServiceProvider services, LoggingService log)
         {
             _commands = commands;
+            _services = services;
             _client = client;
+            _log = log;
         }
 
+        public async Task SetupAsync()
+        {
+            await _commands.AddModulesAsync(Assembly.GetEntryAssembly(), _services);
+            
+            _commands.CommandExecuted += OnCommandExecutedAsync;
+            
+            _client.MessageReceived += HandleCommandAsync;
+        }
         public async Task InstallCommandsAsync()
         {
             _client.MessageReceived += HandleCommandAsync;
 
-
+          
             await _commands.AddModulesAsync(assembly: Assembly.GetEntryAssembly(),
-                                       services: null);
+                                            services: _services);
+        }
+        public async Task OnCommandExecutedAsync(Optional<CommandInfo> command, ICommandContext context, IResult result)
+        {
+            // We can tell the user what went wrong
+            if (!string.IsNullOrEmpty(result?.ErrorReason))
+            {
+                await context.Channel.SendMessageAsync(result.ErrorReason);
+            }
+
+           
+            var commandName = command.IsSpecified ? command.Value.Name : "A command";
+            await _log.LogAsync(new LogMessage(LogSeverity.Info,
+                "CommandExecution",
+                $"{commandName} was executed at {DateTime.UtcNow}."));
         }
         private async Task HandleCommandAsync(SocketMessage messageParam)
         {
@@ -38,7 +64,7 @@ namespace AnnaBot.App
             await _commands.ExecuteAsync(
                 context: context,
                 argPos: argPos,
-                services: null
+                services: _services
                 );
 
         }
---
+++ AnnaBot.App/Startup/Initialize.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnnaBot.App.Startup
+{
+	public class Initialize
+	{
+		private readonly CommandService _commands;
+		private readonly DiscordSocketClient _client;
+
+		public Initialize(CommandService commands = null, DiscordSocketClient client = null)
+		{
+			_commands = commands ?? new CommandService();
+			_client = client ?? new DiscordSocketClient();
+		}
+
+		public IServiceProvider BuildServiceProvider() => new ServiceCollection()
+			.AddSingleton(_client)
+			.AddSingleton(_commands)
+			.AddSingleton<CommandHandler>()
+			.BuildServiceProvider();
+	}
+}

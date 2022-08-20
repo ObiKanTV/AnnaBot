aeeb87b185026e04dfc315387189c9280f6e542e
Kai Tapper <kai.tapper@outlook.com>
2022-08-21 00:02:06 +0200

    Adding Api
    Adding an API Project to the Solution, the idea is that the API will be how we control what's in the bot database when it's up and running.
    
    The API will run the Database for both of the bots, so Twitch and Discord.

---
+++ AnnaBot.Data/Migrations/20220820210335_init.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+
+
+#nullable disable
+
+namespace AnnaBot.Infrastructure.Migrations
+{
+    public partial class init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CustomResponse",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Key = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Value = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    GuildId = table.Column<decimal>(type: "decimal(20,0)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomResponse", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CustomResponse");
+        }
+    }
+}
